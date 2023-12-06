d170adfd51936b73690582e54640b660f81cd7e7
Amit_Valand <143027284+Amitvaland14@users.noreply.github.com>
2023-12-06 14:05:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========== type Ani ========*/
+let typed = new Typed(".typing", {
+    strings:["","Computer Engineer & Web Designer","Web Developer","Computer Engineer"],
+    typeSpeed:150,
+    BackSpeed:100,
+    loop:true
+})
